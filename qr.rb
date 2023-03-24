037aff2e5c70eadc5fc697d8e2a1b705e364e905
Camille Arce <thecamillearce@gmail.com>
2023-03-24 17:01:26 +0000

    QR code for URL added

--- qr.rb
+++ qr.rb
@@ -1,1 +1,10 @@
-p "hello world!"
+require "rqrcode"
+
+# using RQRCode: :QRCode class to encode some text
+qrcode = RQRCode::QRCode.new("https://camille-arce.github.io/")
+
+# Using the .as_png method to create a 500px by 500px image
+png = qrcode.as_png({ :size => 500})
+
+#writing the imade data to a file
+IO.binwrite("theqrcode.png", png.to_s)
